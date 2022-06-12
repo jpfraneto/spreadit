519ddb8855008ef8d2f3f0afd559502c478b62eb
jpfraneto <jpfraneto@gmail.com>
2022-06-12 13:14:02 -0400

    created the api routes for the pooling

--- app.js
+++ app.js
@@ -34,44 +34,8 @@ app.get('/', (req, res) => {
   res.render('landing', { home: { aloja: 46 }, marketIds: data.marketIds });
 });
 
-app.get('/testing', (req, res, next) => {
-  console.log('out of the client');
-});
-
-app.get('/date', (req, res, next) => {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Transfer-Encoding', 'chunked');
-
-  connections.push(res);
-});
-
-app.get('/api', async (req, res, next) => {
-  try {
-    const response = await Promise.all(
-      data.marketIds.map(id =>
-        axios.get(`https://www.buda.com/api/v2/markets/${id}/ticker.json`)
-      )
-    );
-    const marketsInfo = response.map(x => x.data.ticker);
-    const resp = marketsInfo.map(x => {
-      return {
-        id: x.market_id,
-        spread: [
-          Math.floor(Number(x.min_ask[0]) - Number(x.max_bid[0])),
-          x.min_ask[1],
-        ],
-        volume: x.volume,
-        price_variation_24h: x.price_variation_24h,
-        price_variation_7d: x.price_variation_7d,
-      };
-    });
-    res.json({
-      data: { timestamp: new Date().getTime(), markets_ticker: resp },
-    });
-  } catch (error) {
-    console.log('there was an error');
-    console.log(error);
-  }
+app.get('/api/markets', (req, res) => {
+  res.json({ markets: data.marketIds });
 });
 
 app.get('/api/spreads', async (req, res) => {
@@ -86,31 +50,75 @@ app.get('/api/spreads/:marketid', async (req, res) => {
   res.json({ market });
 });
 
-app.post('/api/spreads/:marketid', async (req, res) => {
-  console.log('here is where the pooling will take place');
-  res.json({ 123: 456 });
-});
+app.get('/api/spreads/:marketid/:frequence', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Transfer-Encoding', 'chunked');
 
-app.get('/api/testing', (req, res) => {
-  res.json({ 123: 456 });
-});
+  connections.push(res);
 
-let tick = 0;
+  try {
+    const marketId =
+      data.marketIds.indexOf(req.params.marketid.toUpperCase()) > -1
+        ? req.params.marketid
+        : null;
+    if (!marketId)
+      return res.status(404).json({ message: 'Invalid market id' });
+    const frequence = Number(req.params.frequence.match(/^[0-9]*/)[0]);
+    if (!frequence)
+      return res.status(404).json({
+        message:
+          'Invalid frequence of pooling. The route you are calling should be in the form of /api/spreads/<market_id>/<frequency>, where the frequency parameter is the amount of ms between calls in the pooling',
+      });
+    if (frequence < 55)
+      return res.status(404).json({
+        message:
+          'With such a low frequency you will burn our servers and we will get banned from buda.com. Please increase the frequence of pooling :)',
+      });
+    let market = await functions.fetchBudaForIndividualSpreadInfo(marketId);
+    let baseSpread, newSpread;
+    baseSpread = Number(market.spread[0]);
 
-setTimeout(function run() {
-  if (++tick > LIMIT) {
-    connections.map(res => {
-      res.write('END\n');
-      res.end();
-    });
-    connections = [];
-    tick = 0;
+    let ansObj = {
+      minSpread: baseSpread,
+      maxSpread: baseSpread,
+      spreadComparison: 'initial',
+      timestamp: new Date().getTime(),
+      spread: baseSpread,
+    };
+    res.write(JSON.stringify(ansObj));
+
+    setTimeout(async function run() {
+      market = await functions.fetchBudaForIndividualSpreadInfo(
+        req.params.marketid
+      );
+      newSpread = Number(market.spread[0]);
+
+      ansObj.spread = newSpread;
+      ansObj.timestamp = new Date().getTime();
+
+      if (baseSpread < newSpread) {
+        ansObj.spreadComparison = 'mayor';
+        if (ansObj.maxSpread < newSpread) {
+          ansObj.maxSpread = newSpread;
+          ansObj.extremePoint = true;
+        }
+      } else if (newSpread < baseSpread) {
+        ansObj.spreadComparison = 'menor';
+        if (newSpread < ansObj.minSpread) {
+          ansObj.minSpread = newSpread;
+          ansObj.extremePoint = true;
+        }
+      } else if (baseSpread === newSpread) ansObj.spreadComparison = 'igual';
+
+      res.write(JSON.stringify(ansObj));
+      delete ansObj.extremePoint;
+      setTimeout(run, frequence);
+    }, frequence);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'An error ocurred' });
   }
-  connections.map((res, i) => {
-    res.write(`Hello ${i}! Tick: ${tick} \n`);
-  });
-  setTimeout(run, DELAY);
-}, DELAY);
+});
 
 const port = 3000;
 app.listen(process.env.PORT || port, () => {
